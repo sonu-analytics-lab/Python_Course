cd9c473216cb38fbf0554097e2334ee6e82fcce0
Sonu Singh <56750815+sonu-analytics-lab@users.noreply.github.com>
2026-01-07 15:32:19 +0530

    Add files via upload

---
+++ 02_Python_Fundamentals/05_add.py
@@ -0,0 +1,10 @@
+# a = input("Enter first number") 
+# a = int(a) # convert a to int version of a
+# print(a + 3)
+
+a = int(input("Enter first number"))
+# a = int(a)
+b = int(input("Enter second number"))
+# b = int(b)
+
+print(a + b)
---
+++ 02_Python_Fundamentals/06_comments.py
@@ -0,0 +1,27 @@
+print("Hey how are you?")
+
+# Arjun please finish this code 
+
+
+# Rohan, please review what arjun has done
+'''
+mments are used to explain code and are ignored by the Python
+interpreter.
+Single-line comments start with # .
+Multi-line comments are enclosed in  or """ .
+Escape Sequences
+Escape sequences are used to include special characters in strings.
+Common escape sequences:
+\n : Newline
+\t : Tab
+\\ : Backslash
+\" : Double quote
+\' : Single quote
+Example:
+Print Statement
+The print() function is used to display output.
+•
+•
+•
+# This is a single
+'''
